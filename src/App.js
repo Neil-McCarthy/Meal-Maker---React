2581ca99fccba4d88b233d9ac53e187aaa0d1997
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-10-21 12:44:34 +0100

    starting api with meal maker

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ import './App.css';
 
 function App() {
   const [selectedContent, contentChanger] = useState('Starter');
+  const [mealContent, setMealContent] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMealInfo() {
+    setIsLoading(true);
+    const response = await fetch('https://react-http-5bad7-default-rtdb.europe-west1.firebasedatabase.app/results.json');
+    const data = await response.json();
+    //console.log(data);
+    const transformedMealInfo = data.map(mealData => {
+      return {
+        id: mealData.episode_id,
+        title: mealData.title,
+        openingText: mealData.opening_crawl,
+        releaseDate: mealData.release_date
+      }
+    })
+    setMealContent(transformedMealInfo);
+    setIsLoading(false);
+  }
   return (
     <React.Fragment>
         <header className="App-header">
@@ -18,6 +37,10 @@ function App() {
         </nav>
         <main>
           <ContentStructure content={selectedContent} />
+          <button onClick={fetchMealInfo}>Get info</button>
+          {!isLoading && mealContent.length > 0 && console.log(mealContent[0].title)}
+          {!isLoading && mealContent.length === 0 && console.log('no info')}
+          {isLoading && console.log('loading')}
         </main>
         <aside>
           <PairingList />
