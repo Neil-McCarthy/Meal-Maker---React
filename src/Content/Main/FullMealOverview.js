eb4a92afa983956d8423c4ac757b3fc5bc4f85d2
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-10-29 18:24:13 +0100

    getting images from json working

--- src/Content/Main/FullMealOverview.js
+++ src/Content/Main/FullMealOverview.js
@@ -5,12 +5,10 @@ const FullMealOverview = (props) => {
 
     let seasoningContent = [];
     for (let seasoningData = 0;seasoningData < props.mealInfo.seasoning.length;seasoningData++) {
-        //console.log();
         seasoningContent.push(<tr><td>{props.mealInfo.seasoning[seasoningData]}</td></tr>);
     }
     let ingredientsContent = [];
     for (let ingredientsData = 0;ingredientsData < Object.keys(props.mealInfo.ingredients).length;ingredientsData++) {
-        console.log(Object.values(props.mealInfo.ingredients)[ingredientsData]);
         ingredientsContent.push(<tr><td>{Object.values(props.mealInfo.ingredients)[ingredientsData].name}</td><td>{Object.values(props.mealInfo.ingredients)[ingredientsData].amount}</td></tr>);
     }
     return (
@@ -19,7 +17,7 @@ const FullMealOverview = (props) => {
                 {props.mealInfo.title}
             </h1>
             <p>
-                {props.mealInfo.briefDescription}
+                {props.mealInfo.fullDescription}
             </p>
             <table>
                 <tr>
@@ -40,6 +38,9 @@ const FullMealOverview = (props) => {
                 </tr>
                 {seasoningContent}
             </table>
+            <img src={require(`../../images/mains/Curry/${props.mealInfo.images[0]}`)} />
+            <img src={require(`../../images/mains/Curry/${props.mealInfo.images[1]}`)} />
+            <img src={require(`../../images/mains/Curry/${props.mealInfo.images[2]}`)} />
         </section>
     )
 }
